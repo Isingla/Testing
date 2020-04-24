0c8ac3f6f3985a2c8611f645041c03d7b228209a
Isingla <cheif.ishaan@gmail.com>
2020-04-24 12:27:31 -0400

    added division operator

--- new_python.py
+++ new_python.py
@@ -4,3 +4,11 @@ b=int(input("enter a second number")
 print(a=b)
 print(a-b)
 print(a*b)
+try:
+	value = a/b
+except ZeroDivisionError:
+	print("Not viable option")
+else:
+	print("the division answer is:", value)
+finally:
+	pass
